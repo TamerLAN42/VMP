471ccdf619bef58cc8f5297c67f7d4de94812cbe
TamerLAN <m89137831458@gmail.com>
2022-11-04 11:52:24 +0700

    Диалоговое окно для подтверждения перед вводом данных

--- app/src/main/java/com/example/vmp/MainActivity.java
+++ app/src/main/java/com/example/vmp/MainActivity.java
@@ -4,6 +4,9 @@ import static androidx.core.content.PackageManagerCompat.LOG_TAG;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,10 +14,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -41,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     @Override
    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,51 +56,67 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("RestrictedApi")
             public void onClick(View v) {
+                AlertDialog.Builder aBuilder = new AlertDialog.Builder(MainActivity.this);
+                aBuilder.setMessage("При вводе данных информация в полях очистится");
+                aBuilder.setCancelable(false);
+                aBuilder.setPositiveButton("Да", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick (DialogInterface dialogInterface, int i){
+                      writeFinal();
+                    }
+                });
+                aBuilder.setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                    AlertDialog alert = aBuilder.create();
+                    alert.setTitle("Ввести данные?");
+                    alert.show();
+
+            }
+            public void writeFinal(){
                 final EditText editText = findViewById(R.id.ID1);
                 String name = editText.getText().toString(); // Присвоение переменной значение поля
                 String output = "\",\"p1_fio\":\""+editText.getText().toString();
-                String xren;
                 int year = Calendar.getInstance().get(Calendar.YEAR);
-                    editText.setText("");
+                editText.setText("");
                 EditText editText1 = findViewById(R.id.ID2);
-                            output = output + "\",\"p2_dr\":\"" + editText1.getText().toString();
+                output = output + "\",\"p2_dr\":\"" + editText1.getText().toString();
                 editText1.setText("");
                 editText1 = findViewById(R.id.ID3);
-                            output = output + "\",\"p22_sex\":\"" + editText1.getText().toString();
+                output = output + "\",\"p22_sex\":\"" + editText1.getText().toString();
                 editText1.setText("");
                 editText1 = findViewById(R.id.ID4);
-                            output = output + "\",\"p3_addr_reg\":\"" + editText1.getText().toString();
+                output = output + "\",\"p3_addr_reg\":\"" + editText1.getText().toString();
                 editText1.setText("");
                 editText1 = findViewById(R.id.ID5);
-                            output = output + "\",\"p4_addr_live\":\"" + editText1.getText().toString();
+                output = output + "\",\"p4_addr_live\":\"" + editText1.getText().toString();
                 editText1.setText("");
                 editText1 = findViewById(R.id.ID6);
-                            output = output + "\",\"p5_doc\":\"Паспорт гражданина РФ " + editText1.getText().toString();
+                output = output + "\",\"p5_doc\":\"Паспорт гражданина РФ " + editText1.getText().toString();
                 editText1.setText("");
                 editText1 = findViewById(R.id.ID7);
-                            output = output + "\",\"p6_oms\":\"" + editText1.getText().toString();
+                output = output + "\",\"p6_oms\":\"" + editText1.getText().toString();
                 editText1.setText("");
                 editText1 = findViewById(R.id.ID8);
-                            output = output + "\",\"p7_snils\":\"" + editText1.getText().toString();
+                output = output + "\",\"p7_snils\":\"" + editText1.getText().toString();
                 editText1.setText("");
                 editText1 = findViewById(R.id.ID9);
-                            output = output + "\",\"p8_lgota\":\"" + editText1.getText().toString();
+                output = output + "\",\"p8_lgota\":\"" + editText1.getText().toString();
                 editText1.setText("");
                 editText1 = findViewById(R.id.ID10);
-                            output = output + "\",\"p9_social_grupp\":\"" + editText1.getText().toString();
+                output = output + "\",\"p9_social_grupp\":\"" + editText1.getText().toString();
                 editText1.setText("");
                 editText1 = findViewById(R.id.ID11);
-                            output = output + "\",\"p10_phone\":\"" + editText1.getText().toString();
+                output = output + "\",\"p10_phone\":\"" + editText1.getText().toString();
                 editText1.setText("");
 
-
-
-                    //  output = output + editText1.getText().toString(); // Присвоение переменной значение поля 11 раз
 
 
                 if (!Environment.getExternalStorageState().equals(
                         Environment.MEDIA_MOUNTED)) {
-                    Log.d(LOG_TAG, "SD-карта не доступна: " + Environment.getExternalStorageState());
                     return;
                 }
                 // получаем путь к SD
@@ -110,7 +133,6 @@ public class MainActivity extends AppCompatActivity {
                     bw.write("{\"p00_dd\":\""+ Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + "\",\"p01_mm\":\"" + Calendar.getInstance().get(Calendar.MONTH) + "\",\"p02_yyyy\":\"" + Calendar.getInstance().get(Calendar.YEAR) + "" +output+"\",\"p11_diag_osn\":\"\",\"p12_profile\":\"\",\"p13_vmp_group_num\":\"\",\"p14_vmp_vid_code\":\"\",\"p15_vmp_vid_name\":\"\",\"p16_diag_code_mkb10\":\"\",\"p17_mo_name\":\"\",\"p18_lech_prof_mer\":\"\",\"p19_lab_issl\":\"\",\"p20_instr_issl\":\"\",\"p21_ill_history\":\"\"}");        // пишем данные
                     // закрываем поток
                     bw.close();
-                    Log.d(LOG_TAG, "Файл записан на SD: " + sdFile.getAbsolutePath());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
